ff2686ed435469f27437174c7060d703a2bf7337
suhana <suhanapks@gmail.com>
2025-04-06 19:23:04 +0530

    add to cart

--- src/pages/user/ProductList2.jsx
+++ src/pages/user/ProductList2.jsx
@@ -7,12 +7,22 @@ import { Skeltons } from "../../components/user/Skeltons";
 
 export const ProductList2 = () => {
     const params = useParams();
-    console.log("hiii");
+    // console.log("hiii");
     const [productDetails, isLoading, error] = useFetch(`/products/${params?.id}`);
-    console.log("Fetched product data:", productDetails);
+    // console.log("Fetched product data:", productDetails);
 
 
     const handleAddToCart = async () => {
+        console.log("Product passed to cart:", productDetails?.data);
+        console.log("Product ID:", productDetails?.data?._id);
+        console.log("Add to cart payload:", {
+            product: productDetails?.data?._id,
+            quantity: 1,
+            price: productDetails?.data?.price
+          });
+          
+
+
         if (!productDetails?.data) {
             toast.error("Product not found");
             return;
@@ -44,7 +54,7 @@ export const ProductList2 = () => {
     return (
         <div className="container mx-auto px-4">
             <h1 className="text-2xl font-bold mb-4">Product Details are here </h1>
-            <h1>kikiki</h1>
+            {/* <h1>kikiki</h1> */}
             
                 <div className="card w-96 bg-base-100 shadow-md">
                     <figure>
@@ -61,6 +71,12 @@ export const ProductList2 = () => {
 
                         <button className="btn btn-primary mt-4" onClick={handleAddToCart}>
                             Add to Cart
+                        </button>
+                        <button className="btn btn-primary mt-4" onClick="">
+                            Add to wishlist
+                        </button>
+                        <button className="btn btn-primary mt-4" onClick="">
+                            Buy now
                         </button>
                     </div>
                 </div>

15a27c1a2b3624275930793281c23f0c2f8184a8
suhana <suhanapks@gmail.com>
2025-04-12 20:22:34 +0530

    axio instance commit

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const API_URL=import.meta.env.VITE_API_URL;
-console.log(API_URL);
-export const axiosInstance=axios.create({baseURL:`${API_URL}/api`,withCredentials:true});
+const API_URL = import.meta.env.VITE_API_URL;
 
+console.log("🚀 API URL:", API_URL); // better debug log
 
-
+export const axiosInstance = axios.create({
+  baseURL: `${API_URL}/api`,
+  withCredentials: true, // allow cookies
+});
